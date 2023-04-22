ba143a1d008587a67d8d22503105bea0f5f0836c
ARUN KUMAR <arunkumarbrah@gmail.com>
2023-04-22 14:04:02 +0530

    gitignore added

--- dist/PermifyProvider.d.ts
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from "react";
-export interface UserPayload {
-    id: string;
-    roles: string[];
-    permissions: string[];
-}
-declare const PermifyProvider: ({ children }: PropsWithChildren) => JSX.Element;
-export default PermifyProvider;
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as HasAccess } from "./components/HasAccess";
-export { default as usePermify } from "./hooks/usePermify";
-export { default as PermifyProvider } from "./PermifyProvider";
-export { default as PermifyContext } from "./PermifyContext";
